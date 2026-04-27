b03c602ceb3899a0954d282b110f8c19e6f45e86
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-04-27 13:00:40 -0600

    ejercicio 7 terminado

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Número de máquinas: ");
+int maquinas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Número de turnos: ");
+int turnos = int.Parse(Console.ReadLine());
+
+int[,] produccion = new int[maquinas, turnos];
+
+for (int i = 0; i < maquinas; i++)
+{
+    Console.WriteLine($"Máquina {i}:");
+    for (int j = 0; j < turnos; j++)
+    {
+        Console.Write("Ingrese la producción del turno: ");
+        produccion[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+int PTotal = 0;
+for (int i = 0; i < maquinas; i++)
+{
+    for (int j = 0; j < turnos; j++)
+    {
+        PTotal += produccion[i, j];
+    }
+}
+
+Console.WriteLine($"Producción total de la fábrica: {PTotal}");
