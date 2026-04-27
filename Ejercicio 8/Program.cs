c3e9600d872f64156a7ed602d777c8127ec9613e
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-04-27 13:09:40 -0600

    ejercicio 8 terminado

---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Número de jugadores: ");
+int jugadores = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Número de niveles: ");
+int niveles = int.Parse(Console.ReadLine());
+
+int[,] puntajes = new int[jugadores, niveles];
+
+for (int i = 0; i < jugadores; i++)
+{
+    Console.WriteLine($"Jugador {i}:");
+    for (int j = 0; j < niveles; j++)
+    {
+        Console.Write("Ingrese puntaje del nivel: ");
+        puntajes[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+int mayor = puntajes[0, 0];
+for (int i = 0; i < jugadores; i++)
+{
+    for (int j = 0; j < niveles; j++)
+    {
+        if (puntajes[i, j] > mayor)
+        {
+            mayor = puntajes[i, j];
+        }
+    }
+}
+
+Console.WriteLine($"El puntaje más alto es: {mayor}");
